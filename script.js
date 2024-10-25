3ae6d3c6259bf01bc578647b42fc3bb5be7f67e8
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-10-25 18:03:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const inputField = document.getElementById('fname');
+document.addEventListener('click',()=>{
+       inputField.value =  inputField.value.toUpperCase();
+})
